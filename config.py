59bbe3b7cb31e9f47e801ac1982ae71fc854bd55
jainam3622 <clash.royale20001921@gmail.com>
2024-11-23 18:21:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7331346693:AAFLXgvKd49I5wz6z0YC-wB0paWRV24Qm-o")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@God_x_Sikandar")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1216702307")
+
+
+
+
+
+
+
+  
